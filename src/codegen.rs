a8b7d51287ce0ab45c46667900d9626392b47fdd
Alex Shadley <shadleyalex@gmail.com>
2020-05-17 08:46:29 -0500

    codegen worksgit st!

--- src/codegen.rs
+++ src/codegen.rs
@@ -10,7 +10,8 @@ pub fn gen_prog(out: &mut fs::File, prog: &Prog) {
     }
 
     for def in &prog.definitions {
-        write!(out, "{}", gen_stmt(prog, def)).expect("failed to write line");
+        let (stmt_str, _) = gen_stmt(prog, def);
+        write!(out, "{}", stmt_str).expect("failed to write line");
     }
 
     for func in &prog.functions {
@@ -33,74 +34,118 @@ fn gen_type(prog: &Prog, t: &Type) -> String {
 fn gen_func(prog: &Prog, func: &Func) -> String {
     let mut output = format!("function {}(", gen_sym(&prog.symbol_table, &func.name));
 
+    func.args.first().as_ref().map(|arg| {
+        output = format!("{}{}", output, gen_sym(&prog.symbol_table, arg));
+    });
+    for arg in func.args.iter().skip(1) {
+        output = format!("{}, {}", output, gen_sym(&prog.symbol_table, arg));
+    };
+
     output = format!("{}){{\n", output);
-    output = format!("{}{}", output, gen_body(prog, &func.body));
+    let (body_str, body_val) = gen_body(prog, &func.body);
+    output = format!("{}{}", output, body_str);
+
+    body_val.map(|val| {
+        output = format!("{}return {};\n", output, val);
+    });
 
     format!("{}}}\n", output)
 }
 
-fn gen_body(prog: &Prog, body: &Body) -> String {
+fn gen_body(prog: &Prog, body: &Body) -> (String, Option<String>) {
     let mut output = String::from("");
 
     for stmt in &body.stmts {
-        output = format!("{}{}", output, gen_stmt(prog, &stmt));
+        let (stmt_str, _) = gen_stmt(prog, &stmt);
+        output = format!("{}{}", output, stmt_str);
     }
 
     body.expr.as_ref().map(|expr| {
-        output = format!("return {};\n", gen_expr(prog, &expr));
+        output = format!("{}var _body_val = {};\n", output, gen_expr(prog, &expr));
     });
 
-    output
+    let val_handle = match (&body.expr, body.stmts.last()) {
+        (Some(expr), _) => Some(String::from("_body_val")),
+        (_, Some(stmt)) => {
+            let (_, stmt_val) = gen_stmt(prog, &stmt);
+            stmt_val
+        }
+        (_, _) => None
+    };
+
+    (output, val_handle)
 }
 
-fn gen_stmt(prog: &Prog, stmt: &Stmt) -> String {
+fn gen_stmt(prog: &Prog, stmt: &Stmt) -> (String, Option<String>) {
     match stmt {
         Stmt::Assign(tgt, expr) => {
-            format!("{} = {};\n", gen_target(prog, tgt), gen_expr(prog, expr))
+            let (tgt_str, tgt_var) = gen_target(prog, tgt);
+            (format!("{} = {};\n", tgt_str, gen_expr(prog, expr)), Some(tgt_var))
         }
         Stmt::Case(case) => gen_case(prog, &case),
         Stmt::FnCall(fn_id, args) => {
-            let mut output = format!("{}(", gen_sym(&prog.symbol_table, fn_id).to_owned());
-            for arg in args {
+            let mut output = format!("var _fn_val = {}(", gen_sym(&prog.symbol_table, fn_id).to_owned());
+
+            args.first().as_ref().map(|arg| {
+                output = format!("{}{}", output, gen_expr(prog, arg));
+            });
+            for arg in args.iter().skip(1) {
                 output = format!("{}, {}", output, gen_expr(prog, arg));
-            }
-            format!("{});\n", output)
+            };
+
+            (format!("{});\n", output), Some(String::from("_fn_val")))
         }
     }
 }
 
-fn gen_case(prog: &Prog, case: &Case) -> String {
-    let mut output = format!("var _case_expr{} = {}\n", case.id, gen_expr(prog, &case.expr));
-    output = format!("{}switch(_case_expr[0]){{\n", output);
+fn gen_case(prog: &Prog, case: &Case) -> (String, Option<String>) {
+    let mut output = format!("var _case_expr{} = {};\n", case.id, gen_expr(prog, &case.expr));
+    output = format!("{}var _case_val{};\n", output, case.id);
+    output = format!("{}switch(_case_expr{}[0]){{\n", output, case.id);
 
     for opt in &case.options {
         output = format!("{}{}", output, gen_case_option(&prog, &opt, case.id));
     }
 
-    format!("{}}}\n", output)
+    (format!("{}}}\n", output), Some(format!("_case_val{}", case.id)))
 }
 
 fn gen_case_option(prog: &Prog, option: &CaseOption, case_id: CaseID) -> String {
-    let mut output = format!("case {} :\n", gen_pattern(prog, &option.pattern));
+    let mut output = format!("case {}:\n", gen_pattern(prog, &option.pattern));
 
     for (i, arg) in option.pattern.args.iter().enumerate() {
-        output = format!("{}var {} = _case_expr{}[{}]\n", output, gen_sym(&prog.symbol_table, arg), case_id, i + 1);
+        output = format!("{}var {} = _case_expr{}[{}];\n", output, gen_sym(&prog.symbol_table, arg), case_id, i + 1);
     }
 
-    output
+    match &option.body {
+        CaseBody::Body(body) => {
+            let (body, val_handle) = gen_body(prog, body);
+            output = format!("{}{}", output, body);
+            val_handle.map(|handle| {
+                output = format!("{}_case_val{} = {};\n", output, case_id, handle);
+            });
+        }
+        CaseBody::Expr(expr) => {
+            output = format!("{}_case_val{} = {};\n", output, case_id, gen_expr(prog, expr));
+        }
+    }
+
+    format!("{}break;\n", output)
 }
 
 fn gen_pattern(prog: &Prog, pattern: &CasePattern) -> String {
     gen_adtval(&prog.type_table, &pattern.base)
 }
 
-fn gen_target(prog: &Prog, tgt: &Target) -> String {
+fn gen_target(prog: &Prog, tgt: &Target) -> (String, String) {
     match tgt {
         Target::Mutable(sym) | Target::Var(sym) => {
-            format!("var {}", gen_sym(&prog.symbol_table, sym))
+            let var_name = gen_sym(&prog.symbol_table, sym);
+            (format!("var {}", var_name), var_name)
         }
         Target::Update(sym) => {
-            gen_sym(&prog.symbol_table, sym)
+            let var_name = gen_sym(&prog.symbol_table, sym);
+            (var_name.clone(), var_name)
         }
     }
 }
@@ -116,9 +161,14 @@ fn gen_expr(prog: &Prog, expr: &Expr) -> String {
         Expr::Id(id) => gen_sym(&prog.symbol_table, id),
         Expr::FnCall(fn_id, args) => {
             let mut output = format!("{}(", gen_sym(&prog.symbol_table, fn_id).to_owned());
-            for arg in args {
+
+            args.first().as_ref().map(|arg| {
+                output = format!("{}{}", output, gen_expr(prog, arg));
+            });
+            for arg in args.iter().skip(1) {
                 output = format!("{}, {}", output, gen_expr(prog, arg));
-            }
+            };
+
             format!("{})", output)
         }
         Expr::ADTVal(base, args) => {
--- src/samples/test.sp
+++ src/samples/test.sp
@@ -45,9 +45,38 @@ inc(x) {
     x + 1
 }
 
+type List {
+    Cons(Float, List)
+    Nil
+}
+
+listMap(ls, fn) {
+    case ls {
+        Cons(val, l) -> Cons(fn(val), listMap(l, fn))
+        Nil          -> Nil
+    }
+}
+
+listReduce(fn, start, ls) {
+    case ls {
+        Cons(val, l) -> fn(val, listReduce(fn, start, l))
+        Nil -> start
+    }
+}
+
+add(a, b) {
+    a + b
+}
+
 main() {
     addGlob(10)
 
+    myBool = and(True, False)
+
     mut myVal = Just(5.0)
     myVal := map(myVal, inc)
+
+    mut myList = Cons(1.0, Cons(2.0, Cons(3.0, Nil)))
+    myList := listMap(myList, inc)
+    listReduce(add, 0, myList)
 }
